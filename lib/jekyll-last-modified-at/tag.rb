c7b17b49361e210e0cc862b1edc3538e120ecb9c
bismark39 <donbisoo39@gmail.com>
2022-09-22 11:11:41 +0200

    modifed path

--- lib/jekyll-last-modified-at/tag.rb
+++ lib/jekyll-last-modified-at/tag.rb
@@ -3,15 +3,17 @@
 module Jekyll
   module LastModifiedAt
     class Tag < Liquid::Tag
-      def initialize(tag_name, format, tokens)
+      def initialize(tag_name, format, path, tokens)
         super
         @format = format.empty? ? nil : format.strip
+        @path = path
       end
 
       def render(context)
-        site = context.registers[:site]
-        format = @format || site.config.dig('last-modified-at', 'date-format', 'dir-path')
+        site = context.registers[:site].config['source']
+        format = @format || site.config.dig('last-modified-at', 'date-format')
         article_file = context.environments.first['page']['path']
+        file_path = site_source + '/' + url
         Determinator.new(site.source, article_file, format)
                     .formatted_last_modified_date
       end
